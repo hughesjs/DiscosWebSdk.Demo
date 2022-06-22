4b3874effa6fdc14f3635edd4f1d740646394090
James Hughes <james@pyrosoftsolutions.co.uk>
2022-06-23 00:41:52 +0100

    feat: Added really awful but stupid tiny gateway to keep CORS happy since the no-cors browser mode doesn't let you send auth headers

--- src/DiscosWebSdk.Demo.Blazor/Program.cs
+++ src/DiscosWebSdk.Demo.Blazor/Program.cs
@@ -1,35 +1,17 @@
-using AutoFixture;
-using DiscosWebSdk.Clients;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DiscosWebSdk.Demo.Blazor;
 using DiscosWebSdk.Demo.Blazor.Shared.Services;
-using DiscosWebSdk.Fixtures.AutoFixture.Customizations;
-using DiscosWebSdk.Models.ResponseModels.DiscosObjects;
+using DiscosWebSdk.DependencyInjection;
 using MudBlazor.Services;
-using NSubstitute;
-using NSubstitute.Core;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-Fixture fixture = new();
-fixture.Customize(new DiscosModelFixtureCustomizationNoLinks());
-
-builder.Services.AddTransient(_ =>
-							  {
-								  IDiscosClient client = Substitute.For<IDiscosClient>();
-								  client.GetSingle<DiscosObject>(Arg.Any<string>()).Returns(GetDiscosObject);
-								  return client;
-							  });
-
-async Task<DiscosObject> GetDiscosObject(CallInfo _)
-{
-	await Task.Delay(Random.Shared.Next(1000, 5000));
-	return fixture.Create<DiscosObject>();
-}
-
+builder.Services.AddDiscosServices(builder.Configuration);
 builder.Services.AddTransient<IDiscosModelViewService, DiscosModelViewService>();
 builder.Services.AddScoped(_ => new HttpClient {BaseAddress = new(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddMudServices();
---
+++ src/DiscosWebSdk.Demo.Gateway/Program.cs
@@ -0,0 +1,48 @@
+/* DISCLAIMER:
+ * 
+ * THIS IS AWFUL CODE, YOU SHOULD NOT WRITE CODE LIKE THIS.
+ * IT IS THE BARE MINIMUM REQUIRED TO ACT AS A GATEWAY.
+ * I MADE IT TO ADDRESS THIS: https://stackoverflow.com/a/65762540/4700841
+ * TL;DR - DISCOS DOESN'T SUPPORT CORS. NO-CORS MODE IN BROWSERS DOESN'T LET YOU SEND AUTH HEADERS.
+ * AS SUCH, WE NEED A GATEWAY TO KEEP CORS ON THE BROWSER HAPPY AND PROXY REQUESTS TO DISCOS.
+ * I DIDN'T WANT TO MAKE IT. I DEFINITELY DON'T WANT TO WASTE TIME MAKING THIS GOOD.
+ * DON'T TAKE THIS AS AN EXAMPLE OF HOW I GENERALLY WRITE CODE
+ * IT WILL BREAK IF YOU HIT THE RATE LIMIT
+ *
+ * YOU HAVE BEEN WARNED!
+ */
+
+
+using DiscosWebSdk.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json")
+	   .AddEnvironmentVariables();
+builder.Services.AddCors(options =>
+				 {
+					 options.AddPolicy(name: "cors",
+									   policy =>
+									   {
+										   policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+									   });
+				 });
+
+
+var app = builder.Build();
+app.UseCors("cors");
+
+app.MapGet("/discos-proxy/{*discosRoute}", async (string discosRoute) =>
+										   {
+											   DiscosOptions options = app.Configuration.GetSection("DiscosOptions").Get<DiscosOptions>();
+											   HttpClient    client  = new();
+											   client.BaseAddress                         = new(options.DiscosApiUrl);
+											   client.DefaultRequestHeaders.Authorization = new("bearer", options.DiscosApiKey);
+											   HttpResponseMessage res = await client.GetAsync(discosRoute);
+											   res.EnsureSuccessStatusCode();
+											   Stream             contentStream = await res.Content.ReadAsStreamAsync();
+											   using StreamReader reader        = new(contentStream);
+											   string             content       = await reader.ReadToEndAsync();
+											   return content;
+										   });
+
+app.Run();
